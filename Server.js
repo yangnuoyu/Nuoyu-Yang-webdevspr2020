94c8f68d963f5d351cceadb655f48e08f0fade66
yangnuoyu <yangnuoyu@gmail.com>
2020-02-06 18:05:36 -0800

    update server

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
+
 app.use(express.static('hw1'))
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/hw1/login.html');
@@ -11,9 +12,9 @@ app.get('/', function(req, res) {
 //    res.sendFile(__dirname + '/index.html');
 //});
 //app.use('/public', express.static(__dirname + '/public'))
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+//app.get('/', function(request, response) {
+//  response.send('Hello World!')
+//})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
